aecd50b4956ec7b2c2377740c53589210dbc1668
mayett515 <matthias.vmach@gmail.com>
2024-09-13 09:52:21 +0200

    bring animal data into html

--- animals_web.generator.py
+++ animals_web.generator.py
@@ -9,28 +9,42 @@ animals_data = load_data("animals_data.json")
 
 
 def process_animal_data():
-  # Read the JSON file
-  with open('animals_data.json', 'r') as f:
-    data = json.load(f)
+    # Read the JSON file
+    with open('animals_data.json', 'r') as f:
+        data = json.load(f)
 
-  # Iterate through each animal
-  for animal in data:
-    print("\nName:", animal.get('name'))
+    # Generate a string with the animals' data
+    animal_info = ''
+    for animal in data:
+        name = animal.get('name')
+        characteristics = animal.get('characteristics')
+        locations = animal.get('locations')
 
-    characteristics = animal.get('characteristics')
-    if characteristics:
-      diet = characteristics.get('diet')
-      if diet:
-        print("Diet:", diet)
+        if name:
+            animal_info += f"Name: {name}\n"
 
-      type_ = characteristics.get('type')
-      if type_:
-        print("Type:", type_)
+        if characteristics:
+            diet = characteristics.get('diet')
+            type_ = characteristics.get('type')
+            if diet:
+                animal_info += f"Diet: {diet}\n"
+            if type_:
+                animal_info += f"Type: {type_}\n"
 
-    locations = animal.get('locations')
-    if locations:
-      print("Location:", locations[0])
+        if locations:
+            animal_info += f"Location: {locations[0]}\n"
 
+        animal_info += "\n"
+    # Read the HTML template file
+    with open('animals_template.html', 'r') as f:
+        html_template = f.read()
+
+    # Replace __REPLACE_ANIMALS_INFO__ with the generated string
+    html_output = html_template.replace('__REPLACE_ANIMALS_INFO__', animal_info)
+
+    # Write the new HTML content to a new file
+    with open('animals.html', 'w') as f:
+        f.write(html_output)
 
 # Call the function to process the data
 process_animal_data()
