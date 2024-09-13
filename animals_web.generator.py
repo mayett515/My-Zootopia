5adaa2f07c451e3e77d80da03a1c84e1beaa1c83
mayett515 <matthias.vmach@gmail.com>
2024-09-13 10:04:26 +0200

    generate card item for every animal

--- animals_web.generator.py
+++ animals_web.generator.py
@@ -1,11 +1,29 @@
 import json
 
-def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
 
-animals_data = load_data("animals_data.json")
+def generate_animal_html(animal_data):
+    html = '<li class="cards__item">'
+
+    name = animal_data.get('name')
+    characteristics = animal_data.get('characteristics')
+    locations = animal_data.get('locations')
+
+    if name:
+        html += f"Name: {name}<br/>"
+
+    if characteristics:
+        diet = characteristics.get('diet')
+        type_ = characteristics.get('type')
+        if diet:
+            html += f"Diet: {diet}<br/>"
+        if type_:
+            html += f"Type: {type_}<br/>"
+
+    if locations:
+        html += f"Location: {locations[0]}<br/>"
+
+    html += '</li>'
+    return html
 
 
 def process_animal_data():
@@ -13,38 +31,22 @@ def process_animal_data():
     with open('animals_data.json', 'r') as f:
         data = json.load(f)
 
-    # Generate a string with the animals' data
-    animal_info = ''
+    # Generate HTML content for animals
+    animal_html = ''
     for animal in data:
-        name = animal.get('name')
-        characteristics = animal.get('characteristics')
-        locations = animal.get('locations')
+        animal_html += generate_animal_html(animal)
 
-        if name:
-            animal_info += f"Name: {name}\n"
-
-        if characteristics:
-            diet = characteristics.get('diet')
-            type_ = characteristics.get('type')
-            if diet:
-                animal_info += f"Diet: {diet}\n"
-            if type_:
-                animal_info += f"Type: {type_}\n"
-
-        if locations:
-            animal_info += f"Location: {locations[0]}\n"
-
-        animal_info += "\n"
     # Read the HTML template file
     with open('animals_template.html', 'r') as f:
         html_template = f.read()
 
-    # Replace __REPLACE_ANIMALS_INFO__ with the generated string
-    html_output = html_template.replace('__REPLACE_ANIMALS_INFO__', animal_info)
+    # Replace __REPLACE_ANIMALS_INFO__ with the generated HTML
+    html_output = html_template.replace('__REPLACE_ANIMALS_INFO__', animal_html)
 
     # Write the new HTML content to a new file
     with open('animals.html', 'w') as f:
         f.write(html_output)
+
 
 # Call the function to process the data
 process_animal_data()
