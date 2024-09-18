29b0e3d89c18d0701953a11a2c151517702d333e
mayett515 <matthias.vmach@gmail.com>
2024-09-18 21:17:38 +0200

    new env file and dotenv

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,5 +1,9 @@
 import requests
-my_api_key = "G+cGZpp1wLxy0izuFRuNuw==EKjbtSuWSid8Q1nS"
+#my_api_key = "G+cGZpp1wLxy0izuFRuNuw==EKjbtSuWSid8Q1nS"
+from dotenv import load_dotenv
+import os
+load_dotenv()
+my_api_key = os.getenv("API_KEY")
 def fetch_data(animal_name):
     input_user = animal_name
     api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(input_user)
